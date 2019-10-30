a841518504704859a273b9248deb52295b2b27fb
lukedolamore <48756098+lukedolamore@users.noreply.github.com>
2019-10-30 18:21:24 +1300

    Update sendMeg.py

--- sendMeg.py
+++ sendMeg.py
@@ -6,9 +6,6 @@ def main():
     if len(sys.argv) > 1:
         msg = sys.argv[1]
         sense.show_message(msg, text_colour=[255, 255, 255])#To sent a message to the Sense HAT
-        temp = sense.get_temperature()-20#Getting the current tmp, -20 due to CPU AVG temp
-        temp = round(temp, 1)#Rounding the value to 1 dp
-        print(temp)
 
 if __name__ == "__main__":
     main()
